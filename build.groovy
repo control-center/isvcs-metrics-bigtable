2cf56d5efb6b9e300c0f32f41245ab2305d89121
jplouis <jplouis@gmail.com>
2018-05-23 15:54:42 -0500

    add build script

---
+++ build.groovy
@@ -0,0 +1,23 @@
+pipeline {
+
+    agent {
+        label 'docker-centos-7-4'
+    }
+
+    stages {
+        stage('Build Image'){
+            steps {
+                script {
+                    otsdbimage = docker.build("zenoss/isvcs-metrics-bigtable:v1")
+                }
+            }
+        }
+        stage('Push Image'){
+            steps {
+                script {
+                    otsdbimage.push()
+                }
+            }
+        }
+    }
+}
